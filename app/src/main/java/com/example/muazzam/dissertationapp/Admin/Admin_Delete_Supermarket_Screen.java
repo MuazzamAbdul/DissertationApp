016195a5f4b3714b5f442345417ac08a1b82d284
Muazzam <muazzamabdul@hotmail.com>
2019-02-09 23:26:40 +0400

    Fourteen Commit

---
+++ app/src/main/java/com/example/muazzam/dissertationapp/Admin/Admin_Delete_Supermarket_Screen.java
@@ -0,0 +1,15 @@
+package com.example.muazzam.dissertationapp.Admin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.muazzam.dissertationapp.R;
+
+public class Admin_Delete_Supermarket_Screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin__delete__supermarket__screen);
+    }
+}
--- app/src/main/java/com/example/muazzam/dissertationapp/Admin/Fragments/Fragment_Delete.java
+++ app/src/main/java/com/example/muazzam/dissertationapp/Admin/Fragments/Fragment_Delete.java
@@ -1,13 +1,17 @@
 package com.example.muazzam.dissertationapp.Admin.Fragments;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.muazzam.dissertationapp.Admin.Admin_Delete_Supermarket_Screen;
 import com.example.muazzam.dissertationapp.R;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 
 /**
@@ -25,7 +29,28 @@ public class Fragment_Delete extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fragment__delete, container, false);
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_fragment__delete, container, false);
+        CircleImageView deleteProduct = view.findViewById(R.id.Delete_Product);
+        CircleImageView deleteSupermarket = view.findViewById(R.id.Delete_Supermarket);
+
+        deleteSupermarket.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(),Admin_Delete_Supermarket_Screen.class);
+                startActivity(intent);
+            }
+        });
+
+//        deleteProduct.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(getActivity(),Admin_Add_Product_Screen.class);
+//                startActivity(intent);
+//            }
+//        });
+
+        return view;
     }
 
 }
